9e166bc16d99013dbc39738fd4236b59600b3a62
Ako Jamil <ako.jamil@hotmail.com>
2019-01-14 14:17:48 -0500

    newest version

---
+++ job_background.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ako.jamil@yale.edu
+#SBATCH --partition=day
+#SBATCH --job-name=cluster
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=4000
+#SBATCH --time=10:00:00
+
+python background.py
