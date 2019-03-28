3197cd43c53f033cb333fb02498250b89f854d51
Ako Jamil <ako.jamil@hotmail.com>
2019-03-28 18:15:37 -0400

    update

--- plots.py
+++ plots.py
@@ -3,38 +3,30 @@ import numpy as np
 from matplotlib.ticker import MultipleLocator
 import datetime
 from matplotlib.dates import DateFormatter
+import pandas as pd
 
 def PltTime(time, data, legend, xlabel, ylabel, ylim1=1, ylim2=1, yticks=0):
     fig = plt.figure(figsize=(12,9))
-
     ax = fig.gca()
-
     # ax.minorticks_on()
     # ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(interval=2))
     # ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(interval=60))
     # ax.yaxis.set_major_locator(MultipleLocator(yticks))
     # ax.yaxis.set_minor_locator(MultipleLocator(yticks/2))
-
     ax.grid(b=True, which='major', color='k', linestyle='-')
     ax.grid(b=True, which='minor', color='k', linestyle=':')
-
     plt.xlabel(xlabel, fontsize=14)
     plt.ylabel(ylabel, fontsize=14)
-
     plt.gcf().autofmt_xdate()
     formatter = DateFormatter('%H:%M:%S')
     plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
     data = [x for _,x in sorted(zip(time,data))]
     time = sorted(time)
     timet = [datetime.datetime.strptime(x, '%H%M%S') for x in time]
-
-
-
-
+    plt.xlim(timet[0], timet[-1])
     for yy in data:
-        plt.plot(timet, yy)
-    plt.legend(loc='upper left')
-
+        plt.scatter(timet, yy, label='')
+    plt.legend(legend, loc='upper left')
 
 def PltScatter(xvalue, yvalue, legend, xlabel, ylabel, scale=1.2, xlim=1, xlim2=1, ylim=1, ylim2=1, save=False):
     fig = plt.figure(figsize=(12,7))
@@ -43,25 +35,9 @@ def PltScatter(xvalue, yvalue, legend, xlabel, ylabel, scale=1.2, xlim=1, xlim2=
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     color = ['red', 'blue']
-    print len(xvalue)
-    print len(yvalue)
-    time = [datetime.datetime.strptime(x, '%H%M%S') for x in xvalue]
     for signal in yvalue:
-        print np.shape(xvalue), np.shape(signal)
         plt.scatter(xvalue, signal)
-        # plt.scatter(time, signal)
-        # plt.plot_time(time,signal)
     plt.legend(legend)
-
-def PltScatterD(xvalue, yvalue, yvalue2, legend, xlabel, ylabel, scale=1.2, xlim=1, xlim2=1, ylim=1, ylim2=1, save=False):
-    fig = plt.figure(figsize=(12,7))
-    ax = fig.gca()
-    ax.grid()
-    plt.xlabel('Time [$\mu$s]')
-    plt.ylabel('Amplitude [mV]')
-    plt.scatter(xvalue, yvalue)
-    plt.scatter(xvalue, yvalue2, color='red')
-    plt.legend([label])
 
 def PltWfm(time, data, legend, scale=1.5, xlim=1, xlim2=1, ylim=1, ylim2=1, save=False):
     if(xlim==1 and xlim2==1):
@@ -82,30 +58,6 @@ def PltWfm(time, data, legend, scale=1.5, xlim=1, xlim2=1, ylim=1, ylim2=1, save
         plt.plot(time, signal)
     plt.legend(legend)
 
-def PPltWfm(time, data, data2, label, label2, xlabel, ylabel, scale=1.2, xlim=1, xlim2=1, ylim=1, ylim2=1, save=False):
-    if(xlim==1 and xlim2==1 and ylim==1 and ylim2==1):
-        xlim = time[0]
-        xlim2 = time[-1]
-        ylim = min(min(data),min(data2))*scale
-        ylim2 = max(max(data),max(data2))*scale
-    fig = plt.figure(figsize=(12,7))
-    ax = fig.gca()
-    ax.grid(b=True, which='major', color='k', linestyle='-')
-    ax.grid(b=True, which='minor', color='grey', linestyle=':')
-    ax.xaxis.set_minor_locator(MultipleLocator(100))
-    ax.xaxis.set_major_locator(MultipleLocator(500))
-    ax.yaxis.set_major_locator(MultipleLocator(50))
-    ax.yaxis.set_minor_locator(MultipleLocator(10))
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.xlim(xlim,xlim2)
-    plt.ylim(ylim,ylim2)
-    plt.plot(time, data, color='b', linewidth='1')
-    plt.plot(time, data2, color='r', linewidth='1')
-    plt.legend([label, label2])
-    # plt.show()
-    # plt.savefig("double.pdf")
-
 def plot_single_fft(time, data, data2, label, label2, xlabel, ylabel, xlim, xlim2, ylim, ylim2):
     fig = plt.figure(figsize=(12,7))
     ax = fig.gca()
@@ -117,7 +69,6 @@ def plot_single_fft(time, data, data2, label, label2, xlabel, ylabel, xlim, xlim
     plt.loglog(time, data)
     plt.loglog(time, data2)
     plt.legend(['Cathode', 'Anode','Sum'])
-    # plt.show()
 
 def plot_double_fft(time, freq, data, data2, fft, fft2, label, label2, xlabel, ylabel, xlim, ylim):
     fig = plt.figure(figsize=(12,7))
@@ -166,5 +117,3 @@ def plot_double_fft(time, freq, data, data2, fft, fft2, label, label2, xlabel, y
     plt.xlabel("Frequency [kHz]")
     plt.ylabel("Amplitude")
     plt.semilogy(xf[1:N//2]/1E3, 2.0/N * np.abs(yf4[1:N//2]), '-r')
-
-    # plt.show()
