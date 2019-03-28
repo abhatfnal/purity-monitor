925a2b4acdcd495f6d5b2756e9e88fc4bdb2fa39
Ako Jamil <ako.jamil@hotmail.com>
2019-03-28 18:30:57 -0400

    first

--- job_background.sh
+++ job_background.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=day
 #SBATCH --job-name=cluster
 #SBATCH --ntasks=1 --nodes=1
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=8000
 #SBATCH --time=10:00:00
 
 python background.py
