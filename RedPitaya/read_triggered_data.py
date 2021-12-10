4dca457ceebf637a30d8c5fcce5081d92b36d190
David Moore <david.c.moore@yale.edu>
2021-12-09 20:33:04 -0500

    Updated code to read only segment of buffer

--- RedPitaya/read_triggered_data.py
+++ RedPitaya/read_triggered_data.py
@@ -1,46 +1,61 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-import sys
 import redpitaya_scpi as scpi
 import matplotlib.pyplot as plt
 import struct
-
-num_traces = 100 #number of traces to record
-dec_fac = 1 # decimation factor (see https://redpitaya.readthedocs.io/en/latest/appsFeatures/examples/acqRF-samp-and-dec.html)
-trig_lev_mv = 25 # trigger level in mV
+import numpy as np
+import h5py
 
 rp_s = scpi.scpi('169.254.39.61')
 
-rp_s.tx_txt('ACQ:DATA:FORMAT BIN')
-rp_s.tx_txt('ACQ:DATA:UNITS RAW')
-rp_s.tx_txt('ACQ:DEC %d'%dec_fac)
-rp_s.tx_txt('ACQ:AVG OFF')
+def acquire_file(num_traces=1000, dec_fac=1, trig_lev_mv=25):
+    
+    #num_traces = 100 #number of traces to record
+    #dec_fac = 1 # decimation factor (see https://redpitaya.readthedocs.io/en/latest/appsFeatures/examples/acqRF-samp-and-dec.html)
+    #trig_lev_mv = 25 # trigger level in mV
+    
+    rp_s.tx_txt('ACQ:DATA:FORMAT BIN')
+    rp_s.tx_txt('ACQ:DATA:UNITS RAW')
+    rp_s.tx_txt('ACQ:DEC %d'%dec_fac)
+    rp_s.tx_txt('ACQ:AVG OFF')
+    rp_s.tx_txt('ACQ:TRIG:LEV %d mV'%trig_lev_mv)
+    rp_s.tx_txt('ACQ:TRIG CH1_PE')
+    rp_s.tx_txt('ACQ:TRIG:DLY 8000')
+    rp_s.tx_txt('ACQ:START')
+    
+    i=0
+    data_vec_ch1 = []
+    data_vec_ch2 = []
+    while 1:
+        rp_s.tx_txt('ACQ:TRIG:STAT?')
+        if rp_s.rx_txt() == 'TD':
+            i += 1
+            if(i%100==0): print(i)
+            rp_s.tx_txt('ACQ:SOUR1:DATA:OLD:N? 4096')
+            buff_byte = rp_s.rx_arb()
+            data_vec_ch1.append(buff_byte)
+            rp_s.tx_txt('ACQ:SOUR2:DATA:OLD:N? 4096')
+            buff_byte = rp_s.rx_arb()
+            data_vec_ch2.append(buff_byte)
+            rp_s.tx_txt('ACQ:TRIG CH1_PE;START')
+            if(i>=num_traces): break
+    
+    ch1_dat = []
+    for pidx,d in enumerate(data_vec_ch1):
+        buff = [struct.unpack('!h',bytearray(d[i:i+2]))[0] for i in range(0, len(d), 2)]
+        ch1_dat.append(buff)
+    
+    ch2_dat = []
+    for pidx,d in enumerate(data_vec_ch2):
+        buff = [struct.unpack('!h',bytearray(d[i:i+2]))[0] for i in range(0, len(d), 2)]
+        ch2_dat.append(buff)
+    
+    return np.array(ch1_dat), np.array(ch2_dat)
 
-rp_s.tx_txt('ACQ:TRIG:LEV %d mV'%trig_lev_mv)
-rp_s.tx_txt('ACQ:TRIG CH1_PE')
-rp_s.tx_txt('ACQ:TRIG:DLY 8000')
-rp_s.tx_txt('ACQ:START')
+cd1, cd2 = acquire_file()
+f = h5py.File("mytestfile.h5", "w")
 
-i=0
-data_vec = []
-while 1:
-    rp_s.tx_txt('ACQ:TRIG:STAT?')
-    if rp_s.rx_txt() == 'TD':
-        i += 1
-        print(i)
-        rp_s.tx_txt('ACQ:SOUR1:DATA?')
-        buff_byte = rp_s.rx_arb()
-        data_vec.append(buff_byte)
-        rp_s.tx_txt('ACQ:TRIG CH1_PE;START')
-        if(i>100): break
-
-
-plt.figure()
-for pidx,d in enumerate(data_vec):
-    buff = [struct.unpack('!h',bytearray(d[i:i+2]))[0] for i in range(0, len(d), 2)]
-    if(pidx < 10):
-        plt.plot(buff, label=str(pidx))
-plt.ylabel('Voltage')
-
-plt.show()
+#plt.figure()
+#plt.plot(cd1.T)
+#plt.show()
